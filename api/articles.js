05f3e34cba41cf435c51de07f98f0080625fad16
csbycconnect <csbyc.connect@christuniversity.in>
2026-05-26 09:53:54 +0000

    Added the error catch to find out what error is blocking it

--- api/articles.js
+++ api/articles.js
@@ -1,8 +1,15 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
-console.log("KEY:", process.env.AWS_ACCESS_KEY_ID);
-console.log("REGION:", process.env.AWS_REGION);
+console.log(
+  "ACCESS:",
+  JSON.stringify(process.env.AWS_ACCESS_KEY_ID)
+);
+
+console.log(
+  "SECRET:",
+  JSON.stringify(process.env.AWS_SECRET_ACCESS_KEY)
+);
 
 const client = new DynamoDBClient({
   region: process.env.AWS_REGION,
