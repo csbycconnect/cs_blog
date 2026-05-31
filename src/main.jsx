a2986386bf4438e4cb108f7054461ddc21b3fe97
csbycconnect <csbyc.connect@christuniversity.in>
2026-05-31 04:54:16 +0000

    Rewritting the whole backend let's gooo pain -_- part 67

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/components.css'
 import './styles/blog.css'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { SpeedInsights } from '@vercel/speed-insights'
-import { Analytics } from '@vercel/analytics'
+import { Analytics } from "@vercel/analytics/next"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
